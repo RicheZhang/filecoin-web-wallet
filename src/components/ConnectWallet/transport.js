4055be25fd8a2153a2eb41b08f63e6548341733f
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-04-22 10:58:08 -0400

    Fix ledger issue with HID

--- src/components/ConnectWallet/transport.js
+++ src/components/ConnectWallet/transport.js
@@ -1,32 +1,3 @@
-import TransportWebHID from '@ledgerhq/hw-transport-webhid'
 import TransportWebUSB from '@ledgerhq/hw-transport-webusb'
 
-export default () =>
-  new Promise((resolve, reject) => {
-    TransportWebHID.create()
-      .then(resolve)
-      .catch((err) => {
-        console.log(
-          'error created webHID transport, falling back to webUSB: ',
-          err,
-          err.message
-        )
-        if (
-          err.message.toLowerCase().includes('navigator.hid is not supported')
-        ) {
-          return TransportWebUSB.create()
-        }
-        if (
-          err.message.toLowerCase().includes('transporterror: invalid channel')
-        ) {
-          reject(
-            new Error(
-              'Lost connection with Ledger. Please unplug and replug device.'
-            )
-          )
-        }
-        reject(err)
-      })
-      .then(resolve)
-      .catch(reject)
-  })
+export default () => TransportWebUSB.create()
